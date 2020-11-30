35606a5959c38dada948668b3e0a21af621e1fe2
James Priestley <james@jamespriestley.net>
2020-11-30 02:36:48 +0000

    updates

--- gaussmf/model.py
+++ gaussmf/model.py
@@ -15,15 +15,17 @@ class GaussianMF(BaseEstimator):
     Parameters
     ----------
     n_components : int, optional
-
-    Returns
-    -------
+    max_iter : int, optional
+    tol : float, optional
+    step_size : float, optional
+    components_prior : float, optional
+    samples_prior : float, optional
 
     Attributes
     ----------
-    components_ :
-    log_joint_ :
-    n_iter_ :
+    components_ : array, shape (n_components, n_features)
+    log_joint_ : array, shape (n_iter_)
+    n_iter_ : int
     """
 
     def __init__(self, n_components=2, max_iter=1000, tol=1e-2, step_size=1e-5,
@@ -38,7 +40,6 @@ class GaussianMF(BaseEstimator):
     def _init_components(self, X):
         self.components_ = rng.normal(
             0, 0.1, size=(self.n_components, X.shape[1]))
-        # self._scale_components()
 
     def _scale_components(self):
         self.components_ /= np.linalg.norm(
@@ -50,11 +51,11 @@ class GaussianMF(BaseEstimator):
         gradient = (-1 / self.components_prior) * self.components_ \
             + self._attributes @ error
         self.components_ += self.step_size * gradient
-        # self._scale_components()
 
     def _init_attributes(self, X):
         # attributes matrix is private; it is overwritten during any call to
-        # `self.transform`
+        # `transform`. the user should obtain it via `fit_transform` and
+        # `transform`
         self._attributes = rng.normal(
             0, 0.1, size=(self.n_components, X.shape[0]))
 
@@ -67,17 +68,39 @@ class GaussianMF(BaseEstimator):
     def _calc_elementwise_error(self, X, attributes=None):
         if attributes is None:
             attributes = self._attributes
-
         return X - attributes.T @ self.components_
 
     def reconstruction_error(self, X, attributes=None):
         """Sum of the element-wise squared error between X and the
-        reconstruction given by the current matrix factorization."""
+        reconstruction given by the current matrix factorization. If 'X' is not
+        the matrix used for fitting, you must also pass the attributes matrix.
+
+        Parameters
+        ----------
+        X : array, shape (n_samples, n_features)
+        attributes : array, shape (n_components, n_samples), optional
+
+        Returns
+        -------
+        squared_error : float
+        """
         return (self._calc_elementwise_error(
             X, attributes=attributes) ** 2).sum()
 
-    def _log_proba(self, X, attributes=None):
-        """Compute the log joint probability of the data and parameters"""
+    def log_proba(self, X, attributes=None):
+        """Compute the log joint probability of the data and parameters. If 'X'
+        is not the matrix used for fitting, you must also pass the attributes
+        matrix.
+
+        Parameters
+        ----------
+        X : array, shape (n_samples, n_features)
+        attributes : array, shape (n_components, n_samples), optional
+
+        Returns
+        -------
+        log_joint : float
+        """
         if attributes is None:
             attributes = self._attributes
 
@@ -91,7 +114,6 @@ class GaussianMF(BaseEstimator):
         return log_prior_components + log_prior_attributes + log_likelihood
 
     def _fit(self, X, update_components=False):
-
         self._init_attributes(X)
         if update_components:
             self._init_components(X)
@@ -107,7 +129,7 @@ class GaussianMF(BaseEstimator):
             self._update_attributes(X)
 
             # assess log posterior and check convergence
-            log_joint.append(self._log_proba(X))
+            log_joint.append(self.log_proba(X))
             if i > 0:
                 delta_score = np.diff(log_joint[-2:])
                 if np.abs(delta_score) < self.tol:
@@ -125,22 +147,61 @@ class GaussianMF(BaseEstimator):
         return self.components_, self._attributes
 
     def fit_transform(self, X):
+        """Fit factorization model and return the resulting attributes matrix.
+
+        Parameters
+        ----------
+        X : array, shape (n_samples, n_features)
+
+        Returns
+        -------
+        attributes : array, shape (n_components, n_samples)
+        """
         comp, attr = self._fit(X, update_components=True)
         return attr
 
     def fit(self, X):
+        """Fit factorization model
+
+        Parameters
+        ----------
+        X : array, shape (n_samples, n_features)
+
+        Returns
+        -------
+        self
+        """
         self.fit_transform(X)
         return self
 
     def transform(self, X):
-        """Hold components fixed and iteratively optimize attributes."""
+        """Calculate attributes matrix by optimization, holding the fitted
+        components matrix fixed. Useful for transforming held-out data after
+        fitting the components.
+
+        Parameters
+        ----------
+        X : array, shape (n_samples, n_features)
+
+        Returns
+        -------
+        attributes : array, shape (n_components, n_samples)
+        """
         _, attr = self._fit(X, update_components=False)
         return attr
 
     def score(self, X, attributes=None):
         """Compute the squared reconstruction error of the data given the
-        current components."""
+        current components.
 
+        Parameters
+        ----------
+        X : array, shape (n_samples, n_features)
+
+        Returns
+        -------
+        squared_error : float
+        """
         if attributes is None:
             attributes = self.transform(X)
         return self.reconstruction_error(X, attributes=attributes)
